16dbe7048f7d7872715c5d5761f3b5782b1460f5
Jafet Guzman <jafetguzmanflores@gmail.com>
2023-05-16 23:18:16 -0400

    added font family and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-      template_purple:"hsl(259, 100%, 65%)",
+    colors: {
+      template_purple: "hsl(259, 100%, 65%)",
       template_red: "hsl(0, 100%, 67%)",
+      template_off_white: "hsl(0, 0%, 94%)",
+      template_ligth_grey: "hsl(0, 0%, 86%)",
+      template_smokey_grey: "hsl(0, 1%, 44%)",
+      template_off_black: "hsl(0, 0%, 8%)",
     },
     extend: {},
   },
