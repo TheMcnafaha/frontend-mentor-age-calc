e56d3649e04bdbbeb21d145389d616bb5caca594
Jafet Guzman <jafetguzmanflores@gmail.com>
2023-05-17 01:45:14 -0400

    fixed missing operato on font relsheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,9 @@ export default function Document() {
     <Html>
          <Head>
         
-         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"  />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;1,400;1,800&display=swap"
-          rel="stylesheet"
-        />
+         <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;1,400;1,800&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
