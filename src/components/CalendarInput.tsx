c35ee3243fe41443c248b12490f8c09bfc9d02a3
Jafet Guzman <jafetguzmanflores@gmail.com>
2023-06-29 14:05:23 -0400

    working from default to !default text class
    first step into whole component refactor, still has a lot of unused code

--- src/components/CalendarInput.tsx
+++ src/components/CalendarInput.tsx
@@ -2,51 +2,71 @@ import { TypeInputAge } from "../pages/index";
 import { useState } from "react";
 type CalendarInput = {
   id: "day" | "month" | "year";
-  state: TypeInputAge;
-  errorMessage: String;
   defaultValue: string;
+  errorRange: [number, number];
+  errorMessage: string;
 };
 type OkInput = {
   id: "day" | "month" | "year";
-  state: TypeInputAge;
+  textInput: number;
+  isStart: boolean;
+  nextState: Function;
 };
-type ErrorInput = {
+type DefaultInput = {
   id: "day" | "month" | "year";
-  state: TypeInputAge;
-  errorMessage: String;
+  textInput: "DD" | "MM" | "YYY";
+  setState: Function;
 };
-
 export function CalendarInput({
   id,
   defaultValue,
+  errorRange,
   errorMessage,
 }: CalendarInput) {
   const [textInput, setTextInput] = useState(defaultValue);
   const hasAnyDefaultValue = textInput === defaultValue;
+  const isError = isErrorCheck(textInput, errorRange);
   console.log("le textInput le is me ", textInput);
-
+  let classText =
+    "m-1  ml-[.10rem] cursor-pointer rounded py-1 pl-3 text-xl mix-blend-darken ring-1       ring-template_ligth_grey hover:ring-template_purple  ";
+// else statement must be used because all default  values are also error values, but very much so not the other way around
   if (hasAnyDefaultValue) {
-    console.log("We have a default val: ", hasAnyDefaultValue);
-    return (
-      <DefaultInput
-        id={id}
-        textInput={textInput}
-        setState={setTextInput}
-      ></DefaultInput>
-    );
+    console.log("We got a default");
+    
+    classText =
+      "m-1  ml-[.10rem] cursor-pointer rounded py-1 pl-3 text-xl text-template_smokey_grey  mix-blend-darken ring-1     ring-template_ligth_grey hover:ring-template_purple  ";
   }
-  if (errorMessage !== "") {
-    return (
-      <ErrorInput
-        id={id}
-        textInput={textInput}
-        errorMessage={errorMessage}
-      ></ErrorInput>
-    );
+  else if (isError) {
+    console.log("we got an error");
+    
+    classText =
+      "m-1  ml-[.10rem] cursor-pointer rounded py-1 pl-3 text-xl mix-blend-darken ring-1       ring-template_ligth_grey hover:ring-template_purple  ";
   }
-  return <OkInput id={id} textInput={textInput}></OkInput>;
+  return (
+    <div className="flex w-1/3 flex-col pr-5 ">
+      <label
+        htmlFor={id}
+        className=" text-xs font-light uppercase text-template_smokey_grey"
+      >
+        {id}
+      </label>
+      <input
+        className={classText}
+        type="text"
+        inputMode="numeric"
+        id={id}
+        defaultValue={textInput}
+        key={"sure"}
+        onFocus={hasAnyDefaultValue?()=>{setTextInput("")}:undefined}
+        onChange={(e) => {
+          const nextState = parseInt(e.target.value, 10);
+        }}
+      />
+      <div className="text-xs italic text-template_red">{errorMessage}</div>
+    </div>
+  );
 }
-function DefaultInput({ id, textInput, setState }: OkInput) {
+function DefaultInput({ id, textInput, setState }: DefaultInput) {
   return (
     <div className="flex w-1/3 flex-col pr-5 ">
       <label
@@ -61,7 +81,8 @@ function DefaultInput({ id, textInput, setState }: OkInput) {
         inputMode="numeric"
         id={id}
         defaultValue={textInput}
-        onFocus={(e) => {
+        key={"sure"}
+        onChange={(e) => {
           console.log("halp");
           setState("");
         }}
@@ -69,7 +90,30 @@ function DefaultInput({ id, textInput, setState }: OkInput) {
     </div>
   );
 }
-function OkInput({ id, textInput }: OkInput) {
+function OkInput({ id, textInput, isStart, nextState }: OkInput) {
+  if (isStart) {
+    return (
+      <div className="flex w-1/3 flex-col pr-5 ">
+        <label
+          htmlFor={id}
+          className=" text-xs font-light uppercase text-template_smokey_grey"
+        >
+          {id}
+        </label>
+        <input
+          className="m-1  ml-[.10rem] cursor-pointer rounded py-1 pl-3 text-xl mix-blend-darken ring-1       ring-template_ligth_grey hover:ring-template_purple  "
+          type="text"
+          inputMode="numeric"
+          id={id}
+          defaultValue=""
+          key={"sure"}
+          onChange={(e) => {
+            const nextState = parseInt(e.target.value, 10);
+          }}
+        />
+      </div>
+    );
+  }
   return (
     <div className="flex w-1/3 flex-col pr-5 ">
       <label
@@ -124,4 +168,13 @@ function resetInputValueIfNotDefaultValue(
     return "";
   }
   return input;
+}
+function isErrorCheck(input: any, errRange: [number, number]): boolean {
+  if (!Number.isInteger(input)) {
+    return true;
+  }
+  if (input >= errRange[0] && input < errRange[1]) {
+    return false;
+  }
+  return true;
 }
