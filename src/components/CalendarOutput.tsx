04bb4522e2292b05d416f75445c6a2f6568010b7
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-09-08 20:26:30 -0400

    reverted delay code bc buggy and hacky

--- src/components/CalendarOutput.tsx
+++ src/components/CalendarOutput.tsx
@@ -1,17 +1,16 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { useCountUp } from "react-countup";
 
 type Output = {
   number: number | "--";
   title: string;
-  delay: number;
 };
 type Render = {
   end: number;
-  delay: number;
+  isReset: boolean;
   title: string;
 };
-export const CalendarOutput = ({ number, title, delay = 0 }: Output) => {
+export const CalendarOutput = ({ number, title }: Output) => {
   const isNumber = typeof number === "number";
   if (!isNumber) {
     return (
@@ -20,21 +19,22 @@ export const CalendarOutput = ({ number, title, delay = 0 }: Output) => {
       </>
     );
   }
-
   return (
     <>
-      <MyWay end={number} title={title} delay={delay}></MyWay>
+      <MyWay end={number} title={title}>
+        {" "}
+      </MyWay>
     </>
   );
 };
 
-const MyWay = ({ end, title, delay }: Render) => {
+const MyWay = ({ end, isReset, title }: Render) => {
   const countUpRef = useRef(null);
   const { reset } = useCountUp({
     ref: countUpRef,
-    start: 0,
+    start: 1,
     end: end,
-    delay: delay,
+    delay: 0,
     duration: 4,
     onReset: () => console.log("Resetted!"),
     onUpdate: () => console.log("Updated!"),
